1f1051de557e0d354adb82b2e5abe035ea128ee2
jessesbyers <jesse.s.byers@gmail.com>
2020-02-28 14:06:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, block) {
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(block(element))
+    })
+    return newArray
+} 
+
+function reduce(array, block, value=0) {
+    let answer
+
+    array.forEach(element => {
+       answer = block(element, value)
+    })
+    return answer
+}
+
+// function reduce(array, block, value=0) {
+//     let answer
+//     for (let i = value; i < array.length; i++ ) {
+//         answer = block(array, value)
+//     }
+//     return answer
+// }
