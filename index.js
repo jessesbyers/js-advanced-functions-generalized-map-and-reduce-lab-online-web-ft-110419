dc0e09d0dee7451eff884a06872ceccf37a0ac9f
jessesbyers <jesse.s.byers@gmail.com>
2020-02-28 14:56:27 -0500

    Done.

--- index.js
+++ index.js
@@ -10,25 +10,8 @@ function reduce(array, block, start=0) {
     let value = start
 
     array.forEach(num => {
-       value = block(num, start)
+           value = block(value, num, start)
     })
+    
     return value
 }
-
-
-// function reduceToTotal(array, start=0) {
-//     let value = start
-//     array.forEach(num => {
-//         value = value + num
-//     })
-//     return value
-// }
-
-
-// function reduce(array, block, value=0) {
-//     let answer
-//     for (let i = value; i < array.length; i++ ) {
-//         answer = block(array, value)
-//     }
-//     return answer
-// }
